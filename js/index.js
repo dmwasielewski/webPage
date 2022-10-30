abe1566e2b1ebe13664320e0bc7ce15ee7855954
Damian Wasielewski <wasielewski.damian@gmail.com>
2022-10-30 21:59:38 +0000

    header sticky

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 'use strict';
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+    header.classList.toggle('sticky', this.window.scrollY > 0);
+});
